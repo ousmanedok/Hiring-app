4c81d27241b98ba9f357f630d10ccb1bd9288bf3
unknown <kod.digital@outlook.com>
2022-05-06 16:46:39 +0200

    added the field associated_with

--- main/models.py
+++ main/models.py
@@ -246,20 +246,6 @@ class TeamMember(models.Model):
     def __str__(self):
         return self.name
 
-class Award(models.Model):
-    title = models.CharField(max_length=100)
-    # associated_with = 
-    issuer = models.CharField(max_length=100)
-    issue_date = models.DateTimeField()
-    description = models.TextField()
-    created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="user_awards"
-    )
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
 
 class WorkExperience(models.Model):
     title =  models.CharField(max_length=300)
@@ -277,4 +263,22 @@ class WorkExperience(models.Model):
     updated_date = models.DateTimeField(auto_now=True)   
 
     def __str__(self) :
+        return self.title
+
+
+class Award(models.Model):
+    title = models.CharField(max_length=100)
+    associated_with =  models.ForeignKey(
+        WorkExperience, on_delete=models.CASCADE, related_name="experience_awards"
+    )
+    issuer = models.CharField(max_length=100)
+    issue_date = models.DateTimeField()
+    description = models.TextField()
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_awards"
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
         return self.title
